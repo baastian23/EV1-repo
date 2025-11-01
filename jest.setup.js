f64b21c0416ebddb197292d8f47ed3107e111bc0
SheoInformatic <se.velasquez@duocuc.cl>
2025-11-01 00:08:22 -0300

    test(ci): fix jsdom tests, clean nginx.conf, push GHCR images, lowercase GHCR paths; guard sonar workflow

---
+++ jest.setup.js
@@ -0,0 +1,9 @@
+// Polyfill TextEncoder/TextDecoder for Jest if not present
+const { TextEncoder, TextDecoder } = require('util');
+
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = TextEncoder;
+}
+if (typeof global.TextDecoder === 'undefined') {
+  global.TextDecoder = TextDecoder;
+}
